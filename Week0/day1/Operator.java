6ed892041d0da9f1bac33f4aa9cffd0ef660bdc2
savita SSSS <sumitrathore687@gmail.com>
2024-07-11 14:08:09 +0530

    day

---
+++ Week0/day1/Operator.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Operator {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        a=a+b;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
---
+++ Week0/day1/operators1.java
@@ -0,0 +1,45 @@
+package day1;
+
+public class operators1 {
+    public static void main(String[] args) {
+        // 1. arithematic operators
+       // int a = 2;
+       // int b= 3;
+        // +,-,/,%,++,--
+       // ++a;
+       // a++;
+       // b--;
+       // --b;
+       // b=b-a;
+        // 2. comparison operators
+        // ==,!=,>,<,>=,<=
+        //3 logical operators
+        // &&,||,!
+        //4 assignment operators
+       // int x =1;
+       // x +=3;//x=x+3
+       // x -=3;//x=x-3        x*=3;// x=x*3 
+       // x/=3;//x=x/3
+        //x%=3;//x=x%3
+        //loops : for ,while,do while
+      /*  int sum=0;
+        for ( int i=1;i<=25;i++){
+            sum=sum+i;
+            System.out.print(sum +",");
+        }*/
+        
+        int i= 5;
+        while(i<15){
+
+           if (i%2==0 && i%7==0)
+           System.out.println(i+",");
+           i++;
+
+        }
+
+
+
+    
+    }
+    
+}
